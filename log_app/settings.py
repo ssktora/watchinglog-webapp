ef0834aba5db54cc1ed9be1da456b67af2e248bc
ssktora <sasaki20010626@gmail.com>
2024-01-26 19:05:31 +0900

    データベースも.envへ

--- log_app/settings.py
+++ log_app/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'log_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
